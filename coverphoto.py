e8197616c970dcb5e6df869a78bcd8a7b5d2e68e
Lai <laialexanderholmsen@gmail.com>
2019-05-04 01:12:08 +0200

    Will now default to default_background is no valid image is supplied in BACKGROUND. Restructured some of the code.

--- coverphoto.py
+++ coverphoto.py
@@ -1,3 +1,8 @@
+"""
+A script to automate the creation of cover photos for Facebook
+TODO default to an image if no image is suppplied
+"""
+
 from PIL import Image, ImageDraw, ImageFont
 
 ESN_COLORS = {
@@ -22,29 +27,82 @@ ESN_COLORS = {
         "rgb": (46, 49, 146) #2e3192
     }
 }
+DIMENSIONS = (1568, 588)
+ASPECT_RATIO = DIMENSIONS[0] / DIMENSIONS[1]
+OVERLAY_LOGOS = Image.open("logos_overlay.png")
+BACKGROUND = "bg.jpg"
+DEFAULT_BACKGROUND = "default_background.png"
+TITLE_FONT = ImageFont.truetype("Kelson Sans Bold.otf", 90)
+SUBTITLE_FONT = ImageFont.truetype("Kelson Sans Bold.otf", 50)
+TITLE_V_OFFSET = -6
+SUBTITLE_V_OFFSET = 90
+SUBTITLE2_V_OFFSET = 152
+
+def create_color_overlay(background, color):
+    img = Image.new(background.mode, DIMENSIONS, color["rgb"])
+    return img
+
+def blend_color(background, color):
+    overlay = create_color_overlay(background, color)
+    blended_img = Image.blend(background, overlay, 0.65)
+    return blended_img
+
+def overlay_images(background, overlay):
+    # TODO figure out this mess. I think it's actually most simple to just switch mode. alpha_composite requires both images to have an alpha channel
+    background = background.convert(mode="RGB")
+    # background.mode = "RGB" # necessary to get proper antialiasing on overlay when background is png/rgba
+    background.paste(overlay, (0, 0), overlay)
+    # background = Image.alpha_composite(background, overlay) # this is simpler, and works just as good
+    return background
+
+def overlay_text(background, text, font, offset):
+    draw = ImageDraw.Draw(background)
+    width, height = draw.textsize(text, font)
+    draw.text(((background.size[0] - width) / 2, (background.size[1] - height) / 2 + offset), text, font=font)
+
+def create_coverphoto(background, ext, title, subtitle, subtitle2, color):
+    if not title:
+        title = "cover"
+    background = resize_background(background)
+    cover = overlay_images(blend_color(background, color), OVERLAY_LOGOS)
+    if subtitle or subtitle2:
+        overlay_text(cover, title, TITLE_FONT, TITLE_V_OFFSET)
+        overlay_text(cover, subtitle, SUBTITLE_FONT, SUBTITLE_V_OFFSET)
+        overlay_text(cover, subtitle2, SUBTITLE_FONT, SUBTITLE2_V_OFFSET)
+    else:
+        overlay_text(cover, title, TITLE_FONT, TITLE_V_OFFSET + 25) #offset a bit when only title
+    cover.save(title + "_" + color["name"] + "." + ext, quality=95) #quality only affects jpg images
 
 def open_background_img(filename):
-    background = Image.open(filename)
+    try:
+        background = Image.open(filename)
+        ext = filename.split(".")[-1]
+    except OSError:
+        background = Image.open(DEFAULT_BACKGROUND)
+        ext = DEFAULT_BACKGROUND.split(".")[-1]
+    return background, ext
+
+def resize_background(background):
+    if background.size == DIMENSIONS:
+        return background
     if background.size[0] < DIMENSIONS[0] / 1.33 or background.size[1] < DIMENSIONS[1] / 1.33:
         print("Resolution is low. You will get a better result if you have an image with higher resolution.")
     background_aspect_ratio = background.size[0] / background.size[1]
-    print(background_aspect_ratio)
     # TODO the following could probalby be a function instead of two almost identical blocks of code
-    if background_aspect_ratio <= ASPECT_RATIO:
+    if background_aspect_ratio <= ASPECT_RATIO: # background is taller
         print("bg aspect <= aspect ratio")
+        print(background_aspect_ratio)
         resize_ratio = background.size[0] / DIMENSIONS[0]
         new_height = int(background.size[1] / resize_ratio)
         print(background.size)
         background = background.resize((DIMENSIONS[0], new_height), Image.ANTIALIAS)
         print(background.size)
-        # if original pic is higher
         if background.size[1] > DIMENSIONS[1]:
             padding = (background.size[1] - DIMENSIONS[1]) / 2
             coords = (0, padding, DIMENSIONS[0], background.size[1] - padding)
             background = background.crop(coords)
             print(background.size)
-        # TODO if original pic is lower
-    else:
+    else: # background is wider
         print("bg aspect > aspect ratio")
         resize_ratio = background.size[1] / DIMENSIONS[1]
         new_width = int(background.size[0] / resize_ratio)
@@ -58,53 +116,8 @@ def open_background_img(filename):
             print(background.size)
     return background
 
-DIMENSIONS = (1568, 588)
-ASPECT_RATIO = DIMENSIONS[0] / DIMENSIONS[1]
-OVERLAY_LOGOS = Image.open("logos_overlay.png")
-BG_NAME = "background.png"
-BACKGROUND = open_background_img(BG_NAME)
-TITLE_FONT = ImageFont.truetype("Kelson Sans Bold.otf", 90)
-SUBTITLE_FONT = ImageFont.truetype("Kelson Sans Bold.otf", 50)
-TITLE_V_OFFSET = -6
-SUBTITLE_V_OFFSET = 90
-SUBTITLE2_V_OFFSET = 152
-
-def create_color_overlay(color):
-    img = Image.new(BACKGROUND.mode, DIMENSIONS, color["rgb"])
-    return img
-
-def blend_color(background, color):
-    overlay = create_color_overlay(color)
-    blended_img = Image.blend(background, overlay, 0.65)
-    return blended_img
-
-def overlay_images(background, overlay):
-    # TODO figure out this mess. I think it's actually most simple to just switch mode. alpha_composite requires both images to have an alpha channel
-    # background = background.convert(mode="RGB")
-    background.mode = "RGB" # necessary to get proper antialiasing on overlay when background is png/rgba
-    background.paste(overlay, (0, 0), overlay)
-    # background = Image.alpha_composite(background, overlay) # this is simpler, and works just as good
-    return background
-
-def overlay_text(background, text, font, offset):
-    draw = ImageDraw.Draw(background)
-    width, height = draw.textsize(text, font)
-    draw.text(((background.size[0] - width) / 2, (background.size[1] - height) / 2 + offset), text, font=font)
-
-def create_coverphoto(title, subtitle, subtitle2, color):
-    ext = BG_NAME.split(".")[-1]
-    if not title:
-        title = "cover"
-    cover = overlay_images(blend_color(BACKGROUND, color), OVERLAY_LOGOS)
-    if subtitle or subtitle2:
-        overlay_text(cover, title, TITLE_FONT, TITLE_V_OFFSET)
-        overlay_text(cover, subtitle, SUBTITLE_FONT, SUBTITLE_V_OFFSET)
-        overlay_text(cover, subtitle2, SUBTITLE_FONT, SUBTITLE2_V_OFFSET)
-    else:
-        overlay_text(cover, title, TITLE_FONT, TITLE_V_OFFSET + 25) #offset a bit when only title
-    cover.save(title + "_" + color["name"] + "." + ext, quality=95) #quality only affects jpg images
-
 def run():
+    background, ext = open_background_img(BACKGROUND)
     title = input("Title: ")
     subtitle = input("Subtitle: ")
     subtitle2 = input("Second subtitle: ")
@@ -112,10 +125,10 @@ def run():
     overlay_color = str(overlay_color).lower()
     if overlay_color == "all":
         for color in ESN_COLORS.values():
-            create_coverphoto(title, subtitle, subtitle2, color)
+            create_coverphoto(background, ext, title, subtitle, subtitle2, color)
         return
     overlay_color = ESN_COLORS.get(overlay_color, ESN_COLORS["blue"])
-    create_coverphoto(title, subtitle, subtitle2, overlay_color)
+    create_coverphoto(background, ext, title, subtitle, subtitle2, overlay_color)
     # create_coverphoto("Eventname", "Location", "Date etc", ESN_COLORS["blue"])
 
 run()
